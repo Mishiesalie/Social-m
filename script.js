135608f077b95b1011e03d2e56b74c8ca178ba90
Mishiesalie <mishiesaliey@gmail.com>
2024-07-12 23:39:25 +0300

    script

---
+++ script.js
@@ -0,0 +1,7 @@
+  /* Dark Mode */
+
+ // Select the button
+ const btn = document.getElementById("toggle");
+ // Select the stylesheet <link>
+ const theme = document.querySelector("#theme-link");
+ 
