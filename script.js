53c9cf9494613648397dd738cc30f32e5d1ea6c5
Mishiesalie <mishiesaliey@gmail.com>
2024-11-07 22:22:06 +0300

    update 57

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 
  // Select the button
  const btn = document.getElementById("toggle");
+
+
+ // Select the stylesheet <link>
+const theme = document.querySelector("#theme-link");
+
+// Listen for a click on the button
