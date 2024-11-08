29f5c20d6227bfbe97a4040a38cfd3969cf91375
Mishiesalie <mishiesaliey@gmail.com>
2024-11-08 04:10:14 +0300

    update 58

--- script.js
+++ script.js
@@ -8,3 +8,7 @@
 const theme = document.querySelector("#theme-link");
 
 // Listen for a click on the button
+
+
+btn.addEventListener("click", function() {
+  // If the current URL contains "ligh-theme.css"
